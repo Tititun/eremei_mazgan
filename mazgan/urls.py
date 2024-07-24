8e8ca970eed5d0ddcb932500b0bbccbff8f8353a
danil <vinniopo@mail.ru>
2024-07-24 15:52:25 +0700

    added price_list page

--- mazgan/urls.py
+++ mazgan/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'mazgan'
 
 urlpatterns = [
+    path('price_list', views.price_list, name='price_list'),
     path('', views.main_page, name='main')
 ]
--- mazgan/views.py
+++ mazgan/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpRequest
 from django.shortcuts import render
+from .models import Service
 
 def main_page(request: HttpRequest):
-    return render(request, 'mazgan/main.html')
+    return render(request, 'mazgan/main.html', {'current_page': 'home'})
+
+def price_list(request: HttpRequest):
+    language = request.LANGUAGE_CODE
+    services = Service.objects.filter(translations__language_code=language)
+    return render(request, 'mazgan/price_list.html',
+                  {'services': services, 'current_page': 'price_list'})
