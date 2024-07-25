60ba1ffbc7b4a0241ca44433af5cb77e0c556e9e
danil <vinniopo@mail.ru>
2024-07-25 12:59:24 +0700

    added main form

--- mazgan/forms.py
+++ mazgan/forms.py
@@ -1,7 +1,22 @@
-from django.forms import ModelForm
+from django import forms
+from parler.forms import TranslatableModelForm
 from .models import Bid
 
-class BidForm(ModelForm):
+class BidForm(TranslatableModelForm):
     class Meta:
         model = Bid
         fields = '__all__'
+        widgets = {
+             'email': forms.EmailInput(attrs={
+                     'class': "form-control",
+                     'placeholder': 'optional'
+             }),
+             'phone': forms.TextInput(attrs={
+                 'class': "form-control",
+                 'placeholder': 'optional'
+             }),
+             'text': forms.Textarea(attrs={
+                'class': "form-control",
+                 'rows': '3'
+             }),
+         }
